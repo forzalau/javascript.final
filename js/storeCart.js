789c8895620fa99bd858db73e8996e002138d7c7
Lautaro <forzanolautaro@gmail.com>
2024-02-03 12:21:25 -0300

    push

--- js/storeCart.js
+++ js/storeCart.js
@@ -1,6 +1,29 @@
 let cartHTML = `
-      <h2 class="text-center pt-3"><b>UserName</b> tu carrito esta vacío.</h2>
-      <h2 class="text-center pb-3">¡Empecemos a llenarlo!</h2>      
+  <div class="container">
+    <h4 class="text-center mt-1">MI CARRITO DE COMPRAS</h4>
+    <hr>
+    <div id="compraGracias" class="text-center">
+      <div class="carrito-bg d-flex flex-column align-items-center text-start my-3 p-2" style="background-color: mediumseagreen; border-radius: 5px;">
+        <div id="carrito">
+          <div class="text-center my-3">
+            <h3 class="mb-2">Username tu carrito esta vacío</h3>
+            <h2>¡Empecemos a llenarlo!</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <div class="d-flex justify-content-center">
+      <input type="text" id="cuponIngresar" class="p-1" placeholder="¡Tengo un Cupón!">
+      <button id="cuponValidar" class="ms-1" style="width: auto; background-color: goldenrod; font-weight: bold;">Aplicar</button>
+    </div>
+    <h5 id="cuponAlerta" class="text-center my-3"></h5>
+    <hr>
+    <div class="text-center">
+      <button id="finalizarCompra" class="mt-1" style="width: auto;">FINALIZAR COMPRA</button>
+      <button id="vaciarCarrito" class="mt-1" style="width: auto; background-color: indianred;"><i class="fa-solid fa-trash"></i></button>
+    </div>
+  </div>
 `;
 
 function cart() {
@@ -9,3 +32,147 @@ function cart() {
 }
 
 cart();
+
+const carrito = [];
+let cuponValido = false;
+const descuento = 0.2;
+
+const cuponIngresar = document.getElementById("cuponIngresar");
+const cuponBotonValidar = document.getElementById("cuponValidar");
+const cuponAlerta = document.getElementById("cuponAlerta");
+
+cuponBotonValidar.addEventListener("click", function () {
+  const cuponAplicado = cuponIngresar.value.toLowerCase();
+  cuponValido = cuponAplicado === "20descuento";
+  actualizarCarrito();
+  cuponAlerta.textContent = cuponValido
+    ? "¡Cupón 20% de descuento aplicado correctamente!"
+    : "Cupón inválido, revisa que esté bien escrito.";
+  setTimeout(function () {
+    cuponAlerta.textContent = "";
+  }, 3000);
+});
+
+const productos = [
+  { id: "0", nombre: "J. Walker Blue Label x1", precio: 309990 },
+  { id: "1", nombre: "Cinzano Rosso x6", precio: 27400 },
+  { id: "2", nombre: "Sernova Estilo Italiano x12", precio: 81900 },
+  { id: "3", nombre: "Chandon Extra Brut x6", precio: 38990 },
+  { id: "4", nombre: "Santa Julia Dulce x6", precio: 27880 },
+  { id: "5", nombre: "Coca-Cola x8", precio: 27100 },
+  { id: "6", nombre: "Fernet Branca x6", precio: 57000 },
+  { id: "7", nombre: "Gancia Americano x6", precio: 31990 },
+  { id: "8", nombre: "Smirnoff Clásico x6", precio: 28700 },
+  { id: "9", nombre: "Baron B Extr Brut x6", precio: 81990 },
+  { id: "10", nombre: "Heineken Rubia x6", precio: 11990 },
+  { id: "11", nombre: "Sprite x8", precio: 12990 },
+  { id: "12", nombre: "Jack Daniels x3", precio: 143600 },
+  { id: "13", nombre: "Martini Bianco x6", precio: 27800 },
+  { id: "14", nombre: "Smirnoff Raspberry x6", precio: 28990 },
+  { id: "15", nombre: "Saenz Briones 1888 x6", precio: 24400 },
+  { id: "16", nombre: "Andes Origen Rubia x6", precio: 7700 },
+  { id: "17", nombre: "Hielo Rolito 10kg x3", precio: 6600 },
+  { id: "18", nombre: "Campari x12", precio: 73990 },
+  { id: "19", nombre: "Punt E Mes Vermouth x6", precio: 25990 },
+  { id: "20", nombre: "Sol Azteca Dorado x6", precio: 33400 },
+  { id: "21", nombre: "El Enemigo Malbec x6", precio: 80800 },
+  { id: "22", nombre: "Corona Rubia México x6", precio: 8800 },
+  { id: "23", nombre: "Vasos Descartables x25", precio: 1990 },
+];
+
+function agregarProductoAlCarrito(index) {
+  const productoSeleccionado = productos[index];
+  const indexEnCarrito = carrito.findIndex(
+    (item) => item.producto.id === productoSeleccionado.id
+  );
+  if (indexEnCarrito !== -1) carrito[indexEnCarrito].cantidad++;
+  else carrito.push({ producto: productoSeleccionado, cantidad: 1 });
+  actualizarCarrito();
+  const agregarProductoAlerta = document.getElementById("alerta");
+  agregarProductoAlerta.innerHTML = `<b>${productoSeleccionado.nombre}</b> se agregó al carrito.`;
+  agregarProductoAlerta.style.display = "block";
+  setTimeout(function () {
+    agregarProductoAlerta.style.display = "none";
+  }, 3000);
+}
+
+function restarCantidadDelProducto(idProducto) {
+  const indexEnCarrito = carrito.findIndex(
+    (item) => item.producto.id === idProducto
+  );
+  if (indexEnCarrito !== -1) {
+    if (carrito[indexEnCarrito].cantidad > 1)
+      carrito[indexEnCarrito].cantidad--;
+    else carrito.splice(indexEnCarrito, 1);
+    actualizarCarrito();
+  }
+}
+
+function obtenerDetallesCarrito() {
+  return carrito.map((item) => `${item.producto.nombre} x${item.cantidad}`);
+}
+
+function obtenerTotal() {
+  let total = 0;
+  carrito.forEach((item) => (total += item.producto.precio * item.cantidad));
+  return total;
+}
+
+function obtenerTotalConDescuento() {
+  return obtenerTotal() * (1 - descuento);
+}
+
+function actualizarCarrito() {
+  const verCarrito = document.getElementById("carrito");
+  const totalCarrito = document.getElementById("total");
+  verCarrito.innerHTML = "";
+  const detallesCarrito = obtenerDetallesCarrito();
+  if (detallesCarrito.length === 0) {
+    verCarrito.innerHTML = "Tu carrito está vacío.";
+    totalCarrito.innerHTML = "Total: $ 0";
+  } else {
+    detallesCarrito.forEach((detalle) => {
+      const idProducto = detalle.split(" x")[0];
+      const detalleTexto = document.createElement("span");
+      detalleTexto.textContent = detalle;
+      const botonRestarCantidad = document.createElement("button");
+      botonRestarCantidad.innerHTML = "<i class='fa-solid fa-trash'></i>";
+      botonRestarCantidad.addEventListener("click", function () {
+        restarCantidadDelProducto(idProducto);
+      });
+      verCarrito.appendChild(detalleTexto);
+      verCarrito.appendChild(botonRestarCantidad);
+      verCarrito.appendChild(document.createElement("br"));
+    });
+    totalCarrito.innerHTML = `Total: $ ${
+      cuponValido ? obtenerTotalConDescuento() : obtenerTotal()
+    }`;
+  }
+}
+
+productos.forEach(function (producto, index) {
+  const productoElement = document.getElementById(producto.id);
+  if (productoElement) {
+    productoElement.addEventListener("click", function () {
+      agregarProductoAlCarrito(index);
+    });
+  }
+});
+
+const finalizarCompraBoton = document.getElementById("finalizarCompra");
+finalizarCompraBoton.addEventListener("click", function () {
+  const finalizarCompraGracias = document.getElementById("compraGracias");
+  if (carrito.length === 0) {
+    finalizarCompraGracias.innerHTML =
+      "<h4>Lo sentimos.</h4><br><h6>No se puede finalizar la compra con el carrito vacío.</h6>";
+  } else {
+    finalizarCompraGracias.innerHTML =
+      "<h4>¡Gracias por tu compra!</h4><br><h6>Te enviaremos los detalles de la compra a tu correo electrónico.</h6>";
+  }
+  finalizarCompraGracias.style.display = "block";
+  finalizarCompraBoton.style.display = "none";
+
+  setTimeout(function () {
+    location.reload();
+  }, 5000);
+});
---
+++ js/storeMain.js
@@ -0,0 +1,342 @@
+let storeMainHTML = `
+<div class="row">
+<!-- columna 1 -->
+<div class="col-xl-3">
+  <div id="0" class="card-container sticky">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_993321-MLU72700633653_112023-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>J. Walker Blue Label</h5>
+      <small>x1 unidad</small>
+      <h4 class="pt-2">$ 309.990</h4>
+      <small class="sticky-box float-end">Destacado</small>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_665331-MLA43912239176_102020-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Cinzano Rosso</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 27.400</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_890752-MLA71636397329_092023-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Sernova Estilo Italiano</h5>
+      <small>x12 unidades</small>
+      <h4 class="pt-2">$ 81.900</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_894353-MLA72623463383_112023-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Chandon Extra Brut</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 38.990</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_715393-MLA53163176543_012023-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Santa Julia Dulce</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 27.880</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_909882-MLA43496987906_092020-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Coca-Cola</h5>
+      <small>x8 unidades</small>
+      <h4 class="pt-2">$ 27.100</h4>
+    </div>
+  </div>
+</div>
+<!-- columna 2 -->
+<div class="col-xl-3">
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_938442-MLA73958467108_012024-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>
+        Fernet Branca
+        <small class="offert-box float-end">10% OFF</small>
+      </h5>
+      <small>x6 unidades</small>
+      <h4 class="offer pt-2">$ 57.000</h4>
+      <h5><s>$ 63.700</s></h5>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_917311-MLA70300588050_072023-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Gancia Americano</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 31.990</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_976497-MLA51466323990_092022-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Smirnoff Clásico</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 28.700</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_679018-MLA44154998122_112020-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Baron B Extra Brut</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 81.990</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_916367-MLA50434674376_062022-O.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Heineken Rubia</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 11.990</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_650860-MLA44687481054_012021-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Sprite</h5>
+      <small>x8 unidades</small>
+      <h4 class="pt-2">$ 12.990</h4>
+    </div>
+  </div>
+</div>
+<!-- columna 3 -->
+<div class="col-xl-3">
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_953477-MLA53132721028_012023-O.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>
+        Jack Daniels
+        <small class="offert-box float-end">5% OFF</small>
+      </h5>
+      <small>x3 unidades</small>
+      <h4 class="offer pt-2">$ 143.600</h4>
+      <h5><s>$ 150.400</s></h5>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_755508-MLA70803768741_072023-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Martini Bianco</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 27.800</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_973645-MLA52623878458_112022-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Smirnoff Raspberry</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 28.990</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_942247-MLA69709143009_052023-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Saenz Briones 1888</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 24.400</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_826432-MLA54250536540_032023-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Andes Origen Rubia</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 7.700</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_606841-MLA53101923439_122022-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Hielo Rolito 10kg</h5>
+      <small>x3 unidades</small>
+      <h4 class="pt-2">$ 6.600</h4>
+    </div>
+  </div>
+</div>
+<!-- columna 4 -->
+<div class="col-xl-3">
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_826580-MLA53397244876_012023-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Campari</h5>
+      <small>x12 unidades</small>
+      <h4 class="pt-2">$ 73.990</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_996305-MLU73885153431_012024-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Punt E Mes Vermouth</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 25.990</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_892501-MLA70620881160_072023-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Sol Azteca Dorado</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 33.400</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_726543-MLU71283954969_082023-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>El Enemigo Malbec</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 80.800</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_652365-MLA53244436571_012023-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Corona Rubia México</h5>
+      <small>x6 unidades</small>
+      <h4 class="pt-2">$ 8.800</h4>
+    </div>
+  </div>
+
+  <div id="0" class="card-container">
+    <div class="card-img">
+      <img
+        src="https://http2.mlstatic.com/D_NQ_NP_2X_655896-MLA72137238110_102023-F.webp"
+      />
+    </div>
+    <div class="card-text">
+      <h5>Vasos Descartables</h5>
+      <small>x25 unidades</small>
+      <h4 class="pt-2">$ 1.990</h4>
+    </div>
+  </div>
+</div>
+</div>
+
+  <div id="alerta"></div>
+`;
+
+function storeMain() {
+  let panel = document.getElementById("storeMainContainer");
+  panel.innerHTML = storeMainHTML;
+}
+
+storeMain();
